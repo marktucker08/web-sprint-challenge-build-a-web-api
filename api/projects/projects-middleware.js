a922a3e4cd9ac8be7ec5c46851dccc9487852eb3
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-02-23 20:45:02 -0700

    added post endpoint for projects and created middleware func for validating post request data

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -18,6 +18,18 @@ async function validateProjectId(req, res, next) {
     }
   }
 
+  function validatePost(req, res, next) {
+    // DO YOUR MAGIC
+    const { name, description } = req.body
+    if ( (name !== undefined && typeof name === 'string' && name.length && name.trim().length > 0) || 
+    (description !== undefined && typeof description === 'string' && description.length && description.trim().length > 0)) {
+      next()
+    } else {
+      res.status(400).json({ message: "missing required field" })
+    }
+  }
+
   module.exports = {
     validateProjectId,
+    validatePost
   }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,7 @@
 // Write your "projects" router here!
 const express = require('express')
 const Projects = require('./projects-model')
-const { validateProjectId } = require('./projects-middleware')
+const { validateProjectId, validatePost } = require('./projects-middleware')
 
 const router = express.Router();
 
@@ -15,6 +15,14 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', validateProjectId, (req, res, next) => {
     res.status(200).json(req.prj)
+})
+
+router.post('/', validatePost, (req, res, next) => {
+    Projects.insert(req.body)
+    .then(newPrj => {
+        res.status(200).json(newPrj)
+    })
+    .catch(next)
 })
 
 module.exports = router;
