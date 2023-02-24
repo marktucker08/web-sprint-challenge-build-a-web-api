f591cbfe61a307e81bf96fe8a0d0f4d2afd43b58
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-02-23 20:17:56 -0700

    tasks 1 & 2 complete, established projects router and first get endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+    .then(prj => {
+        res.status(200).json(prj)
+    })
+    .catch(next)
+})
+
+// router.use((err,req,res,next) => {
+//     res.status(500).json({
+//         message: err.message,
+//         stack: err.stack,
+//     })
+// })
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json());
+// server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+server.use((err,req,res,next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
 
 module.exports = server;
